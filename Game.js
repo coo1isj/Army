a390251bd0c47e6bdec00cf30ca06b69a78083e2
coo1isj <coo1piperni@gmail.com>
2016-11-17 15:41:19 -0500

    Added Game.html .css .js

---
+++ Game.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('textarea[data-limit-rows=true]').on('keypress', function (event) {
+        var textarea = $(this),
+            numberOfLines = (textarea.val().match(/\n/g) || []).length + 1,
+            maxRows = parseInt(textarea.attr('rows'));
+
+        if (event.which === 13 && numberOfLines === maxRows) {
+            return false;
+        }
+    });
+});
